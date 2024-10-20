38252b40ed9b70b0b4079044155f0a03ddd90cd4
Kmore1515 <kylebradleymorris@usf.edu>
2024-10-20 13:41:37 -0400

    Calculate total available books in the section

--- library_management.js
+++ library_management.js
@@ -20,19 +20,24 @@ class Book {
 class Section {
     constructor(name) {
     this.name = name;
-    this.books = [];
+    this.books = []; // Open array to accept things being pushed inside
     }
         addBook(book) {
-            this.books.push(book);
+            this.books.push(book); // Adds a new book to the selection
     }   
         listBooks() {
             return this.books.map(book => ({
-                title: book.title,
+                title: book.title, // Lists what books are in the database
                 isAvailable: book.isAvailable
             }));
         }
         getAvailableBooks() {
             return this.books.filter(book => book.isAvailable).length;
+        } // Task 5: Handle Books Borrowing and Returning
+        calculateTotalBooksAvailable() {
+            return this.books.reduce((count, book) => {
+                return book.isAvailable ? count +1 : count; // This makes sure the count is adding +1 when a book is returned.
+            }, 0);
         }
     }
 
@@ -40,28 +45,28 @@ class Section {
 class Patron {
     constructor(name) {
         this.name = name;
-        this.borrowedBooks = [];
+        this.borrowedBooks = []; // This is an open array so that things can be pushed into it.
     }
         borrowBooks(book) {
-            this.borrowedBooks.push(book);
+            this.borrowedBooks.push(book); // Pushes a book into borrowed status and returns a message saying by who and what book.
             return `${book.title} is borrowed by ${this.name}`
         }
         returnBook(book) {
-            book.isAvailable = true;
-            this.borrowedBooks = this.borrowedBooks.filter(book => book !== book);
+            book.isAvailable = true; // Makes the book available again
+            this.borrowedBooks = this.borrowedBooks.filter(book => book !== book); // Puts book back into array while sending a message saying who returned what book,
             return `${book.title} returned by ${this.name}`
         }
 }
 
 // Task 4: Create a VIPPatron Class that Inherits from Patron
-class VIPPatron extends Patron {
+class VIPPatron extends Patron { // Takes the Patron class as a parent class
     constructor(name) {
         super(name)
-        this.priority = true
+        this.priority = true // Makes the VIP a priority
     }
     borrowBooks(book) {
-        book.isAvailable = false;
-        this.borrowedBooks.push(book);
+        book.isAvailable = false; // Takes the book out of availability
+        this.borrowedBooks.push(book); // Pushes book into borrowed and returns a messageing saying who borrowed what book
         return `${book.title} borrowed by VIP ${this.name}`
     }
 }
