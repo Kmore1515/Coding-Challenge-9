0a4834bc727b66aa41de6436f0eaf39897713b77
Kmore1515 <kylebradleymorris@usf.edu>
2024-10-20 13:19:20 -0400

    Create Section class to manage books and availability

--- library_management.js
+++ library_management.js
@@ -5,13 +5,34 @@ class Book {
         this.author = author;
         this.ISBN = ISBN; }
     _isAvailable = true
-    getDetails() { // Returns the title, author, and ISBN
-        return `Book Details: Title: ${this.title}, Author: ${this.author}, ISBN: ${this.ISBN}`;
+        getDetails() { // Returns the title, author, and ISBN
+            return `Book Details: Title: ${this.title}, Author: ${this.author}, ISBN: ${this.ISBN}`;
     }
-    get isAvailable() { // Getter to grab the availability
+        get isAvailable() { // Getter to grab the availability
         return this._isAvailable;
     }
-    set  isAvailable(availability) { // Setter to change the availability if needed
+        set  isAvailable(availability) { // Setter to change the availability if needed
         this._isAvailable = availability;
     }
 }
+
+// Task 2: Create a Section Class
+class Section {
+    constructor(name) {
+    this.name = name;
+    this.books = [];
+    }
+        addBook(book) {
+            this.books.push(book);
+    }   
+        listBooks() {
+            return this.books.map(book => ({
+                title: book.title,
+                isAvailable: book.isAvailable
+            }));
+        }
+        getAvailableBooks() {
+            return this.books.filter(book => book.isAvailable).length;
+        }
+    }
+
